79759d57aad7c6b35882b85edcb7bff03a513038
Caleb Trachte <cetrachte@hotmail.com>
2018-11-14 09:22:46 -0600

    fix selector bug

--- src/components/Book.js
+++ src/components/Book.js
@@ -28,7 +28,7 @@ class Book extends Component {
             </div>
             <div className="book-shelf-changer">
               <select onChange={this.moveBook} value={book.shelf || 'none'}>
-                <option value="none" disabled>Move to...</option>
+                <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
